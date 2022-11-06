f3cc2236a24b6eed54167bf02eca76ebaf4a2703
MoSe <48407417+Merabi1@users.noreply.github.com>
2022-11-06 22:21:29 +0400

    Add project files.

---
+++ Delete Temp Files/Program.cs
@@ -0,0 +1,34 @@
+﻿DirectoryInfo di = new DirectoryInfo(Path.GetTempPath());
+
+
+Task taskDeleteFiles = Task.Run(() =>
+{
+    foreach (FileInfo file in di.GetFiles())
+    {
+        try
+        {
+            file.Delete();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Davichire File Error :" + e.ToString());
+        }
+    }
+});
+
+Task taskDeleteFolders = Task.Run(() => {
+    foreach (DirectoryInfo dir in di.GetDirectories())
+    {
+        try
+        {
+            dir.Delete(true);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Davichire Folder Error : " + e.ToString());
+        }
+    }
+});
+
+taskDeleteFiles.Wait();
+taskDeleteFolders.Wait();
